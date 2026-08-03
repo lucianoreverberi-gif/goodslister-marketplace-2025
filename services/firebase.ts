7c0445b4101e575a78d07713ba305ba919fb90f6
lucianoreverberi-gif <lucianoreverberi@gmail.com>
2026-08-03 16:49:17 -0400

    fix: point Firestore SDK to named default database

--- services/firebase.ts
+++ services/firebase.ts
@@ -29,7 +29,7 @@ import {
 import firebaseConfig from '../firebase-applet-config.json';
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+export const db = getFirestore(app, 'default');
 
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
