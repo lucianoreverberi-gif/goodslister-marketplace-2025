ad32f3e2398578d47501b86863dffecc418e95fd
lucianoreverberi-gif <lucianoreverberi@gmail.com>
2026-08-12 14:44:05 -0400

    fix: close flex-col div before Banners section to balance JSX

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -612,7 +612,7 @@ const HomePage: React.FC<HomePageProps> = ({
                 {/* FAQ Section */}
                 <FAQSection />
 
-                {/* Banners */}
+                </div>{/* Banners */}
                 <div className="py-16 sm:py-24">
                     <div className="container mx-auto px-4 sm:px-6 lg:px-8 flex flex-col gap-12">
                         {banners.map(banner => renderBanner(banner))}
